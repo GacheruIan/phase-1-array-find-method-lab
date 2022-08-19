3eef3b2991d91e2c774a14200f923db441079b41
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-19 20:00:19 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+
+    function superbowlWin(record)
+
+{
+    // const found = array1.find(element => element > 10); -- syntax
+    let result = record.find( record => record.result === "W" );
+
+      // const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+    const  sadReality =  result ? result.year : undefined;
+    return sadReality;
+
+  
+}
+
+superbowlWin()
+
